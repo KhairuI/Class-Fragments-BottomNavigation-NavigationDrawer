47cd932a1bf742c75c77479d6b7878988370dea9
Khairul Islam <khairul4166@gmail.com>
2020-11-20 22:41:43 +0600

    First Commit

---
+++ app/src/main/java/com/example/class_fragments/activity/DrawerActivity.java
@@ -0,0 +1,16 @@
+package com.example.class_fragments.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.class_fragments.R;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
